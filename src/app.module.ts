a4d7cac1fadd3ef45bea5a5635b6222dbedaf5ac
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-02-11 17:41:26 -0300

    feature: Add Prisma basic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
